e1cfd183fdbebe8ef428c428f4e618b5a9bae375
Softanitha <bokkaanitha6@gmail.com>
2024-08-01 17:59:05 +0530

    first commit

--- src/main/java/com/example/simple/SimpleApplication.java
+++ src/main/java/com/example/simple/SimpleApplication.java
@@ -2,9 +2,10 @@ package com.example.simple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SimpleApplication {
+public class SimpleApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleApplication.class, args);
