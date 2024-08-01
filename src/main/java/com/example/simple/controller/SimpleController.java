7492fcc3120a7c12fd29f2d7601ab9f3896f2a5c
Softanitha <bokkaanitha6@gmail.com>
2024-08-01 19:02:11 +0530

    Update SimpleController.java

--- src/main/java/com/example/simple/controller/SimpleController.java
+++ src/main/java/com/example/simple/controller/SimpleController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SimpleController {
 	
-	@GetMapping("/hello")
+	@GetMapping("/")
 	public String getMessage() {
 		
 		return "Welcome to my Simple Application";
